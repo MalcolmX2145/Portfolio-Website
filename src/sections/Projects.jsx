9d1a1c8d32871e527b64a752637a71d7dc8c3a5a
MalcolmX2145 <ekajjamalcolm@gmail.com>
2025-03-15 17:32:43 +0300

    Updated Product Store project

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -11,7 +11,7 @@ import { FaLink, FaGithub } from "react-icons/fa";
 import NotesApp from "../assets/noteapp.jpg";
 import MovieApp from "../assets/movieapp.jpg";
 import ExpenseTracker from "../assets/expense_tracker.jpg";
-import ProductStore from "../assets/productstore.png";
+import ProductStore from "../assets/product_store.png";
 import { Link } from "react-router-dom";
 
 const Projects = () => {
@@ -45,13 +45,13 @@ const Projects = () => {
     {
       image: ProductStore,
       title: "Product Store App",
-      demoLink: "https://react-product-store.onrender.com/",
-      codeLink: "https://github.com/MalcolmX2145/react_product_store",
+      demoLink: "https://product-store-v2.onrender.com/",
+      codeLink: "https://github.com/MalcolmX2145/product-store-v2",
       techStack: [
         { name: "ReactJS" },
         { name: "TailwindCSS" },
         { name: "Express.js" },
-        { name: "MongoDB" },
+        { name: "PostgreSQL" },
       ],
     },
   ];
